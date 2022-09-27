05c239f3f4f690e2536311f23c856a22eb1f7d8c
OA Wu <oawu.tw@gmail.com>
2022-09-27 18:29:20 +0800

    Fix.

--- src/js/index.js
+++ src/js/index.js
@@ -5,6 +5,172 @@
  * @link        https://www.ioa.tw/
  */
 
+// iOS, iPadOS
+  // Red systemRed
+  // 255,  59,  48
+  // 255,  69,  58
+
+  // Orange  systemOrange
+  // 255, 149,   0
+  // 255, 159,  10
+
+  // Yellow  systemYellow
+  // 255, 204,   0
+  // 255, 214,  10
+
+  // Green systemGreen
+  //  52, 199,  89
+  //  48, 209,  88
+
+  // Mint  systemMint
+  //   0,   199, 190
+  // 102, 212, 207
+
+  // Teal  systemTeal
+  //  48, 176, 199
+  //  64, 200, 224
+
+  // Cyan  systemCyan
+  //  50, 173, 230
+  // 100, 210, 255
+
+  // Blue  systemBlue
+  //  0, 122, 255
+  // 10, 132, 255
+
+  // Indigo  systemIndigo
+  //  88,  86, 214
+  //  94,  92, 230
+
+  // Purple  systemPurple
+  // 175,  82, 222
+  // 191,  90, 242
+
+  // Pink  systemPink
+  // 255,  45,  85
+  // 255,  55,  95
+
+  // Brown systemBrown
+  // 162, 132,  94
+  // 172, 142, 104
+
+  // Gray  systemGray
+  // 142, 142, 147
+  // 142, 142, 147
+
+  // Gray (2)  systemGray2
+  // 174, 174, 178
+  //  99,  99, 102
+
+  // Gray (3)  systemGray3
+  // 199, 199, 204
+  //  72,  72,  74
+
+  // Gray (4)  systemGray4
+  // 209, 209, 214
+  //  58,  58,  60
+
+  // Gray (5)  systemGray5
+  // 229, 229, 234
+  //  44,  44,  46
+
+  // Gray (6)  systemGray6
+  // 242, 242, 247
+  //  28,  28,  30
+
+// macOS
+  // Red systemRedColor
+  // 215,   0,  21
+  // 255, 105,  97
+
+  // Orange  systemOrangeColor
+  // 201,  52,   0
+  // 255, 179,  64
+
+  // Yellow  systemYellowColor
+  // 160,  90,   0
+  // 255, 212,  38
+
+  // Green systemGreenColor
+  //   0, 125,  27
+  //  49, 222,  75
+
+  // Mint  systemMintColor
+  //  12, 129, 123
+  // 102, 212, 207
+
+  // Teal  systemTealColor
+  //   0, 130, 153
+  //  93, 230, 255
+
+  // Cyan  systemCyanColor
+  //   0, 113, 164
+  // 112, 215, 255
+
+  // Blue  systemBlueColor
+  //   0,  64, 221
+  //  64, 156, 255
+
+  // Indigo  systemIndigoColor
+  //  54,  52, 163
+  // 125, 122, 255
+
+  // Purple  systemPurpleColor
+  // 173,  68, 171
+  // 218, 143, 255
+
+  // Pink  systemPinkColor
+  // 211,  15,  69
+  // 255, 100, 130
+
+  // Brown systemBrownColor
+  // 127, 101,  69
+  // 181, 148, 105
+
+  // Gray  systemGrayColor
+  // 105, 105, 110
+  // 152, 152, 157
+
+// watchOS
+  // Red systemRed
+  // 255,  59,  48
+
+  // Orange  systemOrange
+  // 255, 149,  0
+
+  // Yellow  systemYellow
+  // 255, 230,  32
+
+  // Green systemGreen
+  //   4, 222, 113
+
+  // Mint  systemMint
+  // 102, 212, 207
+
+  // Teal  systemTeal
+  // 106, 196, 220
+
+  // Cyan  systemCyan
+  //  90, 200, 250
+
+  // Blue  systemBlue
+  //  32, 148, 250
+
+  // Indigo  systemIndigo
+  // 120, 122, 255
+
+  // Purple  systemPurple
+  // 191,  90, 242
+
+  // Pink  systemPink
+  // 250,  17,  79
+
+  // Brown systemBrown
+  // 172, 142, 104
+
+  // Gray  systemGray
+  // 155, 160, 170
+
 
 Load.Vue({
   data: {
@@ -40,13 +206,14 @@ Load.Vue({
       }, {
         name: 'color', title: '顏色', colors: [
           { light: [   0, 122, 255, 1.0 ], dark: [  10, 132, 255, 1.0 ], name: 'systemBlue' },
+
           { light: [  52, 199,  89, 1.0 ], dark: [  48, 209,  88, 1.0 ], name: 'systemGreen' },
           { light: [  88,  86, 214, 1.0 ], dark: [  94,  92, 230, 1.0 ], name: 'systemIndigo' },
           { light: [ 255, 149,   0, 1.0 ], dark: [ 255, 159,  10, 1.0 ], name: 'systemOrange' },
           { light: [ 255,  45,  85, 1.0 ], dark: [ 255,  55,  95, 1.0 ], name: 'systemPink' },
           { light: [ 175,  82, 222, 1.0 ], dark: [ 191,  90, 242, 1.0 ], name: 'systemPurple' },
           { light: [ 255,  59,  48, 1.0 ], dark: [ 255,  69,  58, 1.0 ], name: 'systemRed' },
-          { light: [  90, 200, 250, 1.0 ], dark: [ 100, 210, 255, 1.0 ], name: 'systemTeal' },
+          { light: [  48, 176, 199, 1.0 ], dark: [  64, 200, 224, 1.0 ], name: 'systemTeal' },
           { light: [ 255, 204,   0, 1.0 ], dark: [ 255, 214,  10, 1.0 ], name: 'systemYellow' },
         ],
       }, {
